737d7d9028ba17ffbcc096ff51cdbf616b21c7ac
somestuff <57671850+HelloKindGit@users.noreply.github.com>
2021-12-01 14:42:25 +0100

    test

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1 class="text-center">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</h1>
+  </div>
+</template>
---
+++ src/views/Rezepte.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="about">
+    <h1 class="text-center">Rezept Liste</h1>
+
+    <table class="table table-striped">
+      <thead>
+      <tr>
+        <th>Rezept Id</th>
+        <th>Rezept Name</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="rezept in rezepte" v-bind:key="rezept.id">
+        <td>{{ rezept.id }}</td>
+        <td>{{ rezept.name }}</td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import RezeptService from '../services/RezeptService'
+
+export default {
+  name: 'Rezepte',
+  data () {
+    return {
+      rezepte: []
+    }
+  },
+  methods: {
+    getRezepte () {
+      RezeptService.getRezepte().then((response) => {
+        this.rezepte = response.data
+      })
+    }
+  },
+  created () {
+    this.getRezepte()
+  }
+}
+</script>
