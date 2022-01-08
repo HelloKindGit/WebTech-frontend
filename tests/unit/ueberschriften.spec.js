53355a6dede17d00031b984fd0855beeff01a7ba
Dominik Wache <dominik.wache@web.de>
2022-01-08 01:13:31 +0100

    Test bugs 4

--- tests/unit/ueberschriften.spec.js
+++ tests/unit/ueberschriften.spec.js
@@ -3,14 +3,14 @@ import Rezepte from '@/views/Rezepte'
 import CreateRezept from '@/views/CreateRezept'
 
 describe('Testing Rezepte.vue', () => {
-  it('Sollte die korrekte Überschrift haben', () => {
+  it('Sollte die korrekte Überschrift haben.', () => {
     const wrapper = mount(Rezepte)
     expect(wrapper.text()).toMatch('Rezepte')
   })
 })
 
 describe('Testing CreateRezept.vue', () => {
-  it('Sollte die korrekte Überschrift haben', () => {
+  it('Sollte die korrekte Überschrift haben.', () => {
     const wrapper = mount(CreateRezept)
     expect(wrapper.text()).toMatch('RezeptName: Okay! Beschreibung: Okay! Vorbereitungszeit: Gib eine Zeit an! ' +
       'Kochzeit: Gib eine Zeit an! Portionen: Gib die Anzahl der Portionen an! Kategorie: Gib eine Kategorie an! Upload Rezept')
