84f06d3427614c0ed75cf5e75d24cdb3e1e5364b
Dominik Wache <dominik.wache@web.de>
2022-01-09 15:36:21 +0100

    wrong authorization link

--- src/auth/authorization.js
+++ src/auth/authorization.js
@@ -1,12 +1,11 @@
 import OktaSignIn from '@okta/okta-signin-widget'
 import { OktaAuth } from '@okta/okta-auth-js'
-/*
 import { FRONTEND_BASE_URL } from '@/config'
-*/
+
 const oktaSignIn = new OktaSignIn({
   baseUrl: 'https://dev-80022801.okta.com',
   clientId: '0oa3igwin4hVN0Wlp5d7',
-  redirectUri: 'http://localhost:3000/login/callback',
+  redirectUri: FRONTEND_BASE_URL + '/login/callback',
   authParams: {
     pkce: true,
     issuer: 'https://dev-80022801.okta.com/oauth2/default',
