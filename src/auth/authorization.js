1edafa9e2724a7ef0f7a7140fd4ab9bed6429fe2
Dominik Wache <dominik.wache@web.de>
2022-01-09 16:30:20 +0100

    authorization link bug #3

--- src/auth/authorization.js
+++ src/auth/authorization.js
@@ -1,11 +1,10 @@
 import OktaSignIn from '@okta/okta-signin-widget'
 import { OktaAuth } from '@okta/okta-auth-js'
-import { FRONTEND_BASE_URL } from '@/config'
 
 const oktaSignIn = new OktaSignIn({
   baseUrl: 'https://dev-80022801.okta.com',
   clientId: '0oa3igwin4hVN0Wlp5d7',
-  redirectUri: FRONTEND_BASE_URL + '/login/callback',
+  redirectUri: 'https://webtech-rezeptbuch-frontend.herokuapp.com/login/callback',
   authParams: {
     pkce: true,
     issuer: 'https://dev-80022801.okta.com/oauth2/default',
@@ -18,7 +17,7 @@ const oktaSignIn = new OktaSignIn({
 const oktaAuth = new OktaAuth({
   issuer: 'https://dev-80022801.okta.com/oauth2/default',
   clientId: '0oa3igwin4hVN0Wlp5d7',
-  redirectUri: FRONTEND_BASE_URL + '/login/callback',
+  redirectUri: window.location.origin + '/login/callback',
   scopes: ['openid', 'profile', 'email']
 })
 
